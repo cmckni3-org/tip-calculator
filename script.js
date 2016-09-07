8d2bd9171ce78ae991d01488e3dda8b6ddb2e07e
Chris McKnight <cmckni3@gmail.com>
2016-09-06 21:37:34 -0500

    Switches to bootstrap 4

--- script.js
+++ script.js
@@ -2,27 +2,40 @@ function baseCost()
 {
   return 100 * parseInt(document.getElementById('base-cost').value, 10);
 }
+
 function tip()
 {
   return parseInt(document.getElementById('tip-percentage').value, 10) / 100;
 }
+
 function tax()
 {
   return parseInt(document.getElementById('tax-rate').value, 10) / 100;
 }
+
 function subtotal()
 {
   return baseCost() * (tax() + 1);
 }
+
 function total()
 {
   return Math.floor(subtotal() * (tip() + 1)) / 100;
 }
+
 function calculate()
 {
-  document.getElementById('total').innerHTML = '$' + total();
-  return total();
+  var base_cost = parseInt(document.getElementById('base-cost').value, 10);
+  var tip_rate  = parseInt(document.getElementById('tip-percentage').value, 10);
+  var tax_rate  = parseInt(document.getElementById('tax-rate').value, 10);
+  if (base_cost && tip_rate && tax_rate) {
+    document.getElementById('total').innerHTML = '$' + total();
+    return total();
+  } else {
+    document.getElementById('total').innerHTML = 'Enter values for base cost, tip and tax.';
+  }
 }
+
 window.onload = function()
 {
   document.getElementById('submit').onclick = calculate;
